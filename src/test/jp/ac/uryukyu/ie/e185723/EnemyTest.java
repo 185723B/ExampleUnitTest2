e05d6970f06748910d505caac2ff11775879a4a3
yusuke shimabukuro <e185723@ie.u-ryukyu.ac.jp>
2018-11-15 14:47:38 +0900

    report3

---
+++ src/test/jp/ac/uryukyu/ie/e185723/EnemyTest.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e185723;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void attack() {
+    }
+}
