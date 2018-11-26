7436e0f8dd9cf6114050b46ef8674fc624aab4c8
yusuke shimabukuro <e185723@ie.u-ryukyu.ac.jp>
2018-11-26 14:51:43 +0900

    report3

--- src/main/jp/ac/uryukyu/ie/e185723/Main.java
+++ src/main/jp/ac/uryukyu/ie/e185723/Main.java
@@ -4,14 +4,14 @@ package jp.ac.uryukyu.ie.e185723;
  * Created by tnal on 2016/11/13.
  */
 public class Main {
-    public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
-        Enemy enemy = new Enemy("スライム", 6, 3);
+    public static void main(String[] args) {
+        Hero hero = new Hero();
+        Enemy enemy = new Enemy();
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
         int turn = 0;
-        while( !hero.isDead()  && !enemy.isDead()  ){
+        while (!hero.isDead() && !enemy.isDead()) {
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
